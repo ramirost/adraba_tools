9bee9496630847e7eddca04340c69c507a38d708
Ramiro Sanchez <ramirost@gmail.com>
2020-05-29 21:04:01 +0200

    Primera versión

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# adraba_tools
+# Instalación
+
+## Dependencias 
+Para la instalación es necesario tener instalado `git` y `curl` (ya deberían estar en cualquier Mac moderno)
+
+Instalamos `nvm`, la versión 12.16.3 de `node` y `yarn`
+
+```
+# Instalación nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# Instalación node
+nvm install 12.16.3
+
+# Configuración de node por defecto
+nvm alias default 12.16.3
+
+# Instalación yarn
+npm install -g yarn
+```
+
+## Instalación herramienta
+
+```
+cd ~
+git clone https://github.com/ramirost/adraba_tools
+```
+
+# Actualización
+
+Si hay alguna actualización hay que situarse en el directorio donde se haya descargado y ejecutar:
+
+```
+cd ~/adraba_tools
+git pull
+```
+
+# Uso
+
+Antes del primer uso en el directorio donde se haya descargado ejecutar.
+
+```
+cd ~/adraba_tools
+yarn
+```
+
+Mientras no haya cambios en el código del programa para generar un informe ejecutar.
+
+```
+cd ~/adraba_tools
+yarn c3 --entrada directorioDeArchivosMail --salida archivo.csv
+```
+
+Ejemplo con la muestra incluida en el repositorio:
+
+```
+cd ~/adraba_tools
+yarn c3 --entrada ~/adraba_tools/samples/ --salida ~/ejemplo_c3.csv
+```
+
+El resultado estará en el archivo `ejemplo_c3.csv` del directorio del usuario.
---
+++ src/c3.ts
@@ -0,0 +1,162 @@
+import * as yargs from "yargs";
+import * as stream from 'stream';
+import * as readline from 'readline';
+import * as fs from 'fs';
+import * as util from 'util';
+import * as path from 'path';
+import * as dayjs from 'dayjs';
+import * as papa from 'papaparse';
+import customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(customParseFormat);
+
+const stat = util.promisify(fs.stat);
+const exists = util.promisify(fs.exists);
+const readdir = util.promisify(fs.readdir);
+
+async function main(): Promise<number> {
+  const argv = yargs
+    .example('yarn c3 --entrada ~/informes_c3 --salida ~/informe_c3_generado.csv', 'Crea informe CSV a partir de archivos de texto')
+    .demand('entrada')
+    .demand('salida')
+    .argv as {
+      entrada: string;
+      salida: string;
+    };
+
+  const inputFolder = argv.entrada;
+  const outputFile = argv.salida;
+  if (!await folderExists(inputFolder, "entrada")) {
+    return 1;
+  }
+  if (await fileExists(outputFile)) {
+    return 1;
+  }
+  const output = fs.createWriteStream(outputFile);
+  let headersDone = false;
+  for await (const file of folderFiles(inputFolder)) {
+    for await (const entry of parseFile(file)) {
+      if (!headersDone) {
+        await dumpToCSV(output, papa.unparse({
+          fields: getHeaders(),
+          data: [entryToArray(entry)]
+        }, getPapaOptions()));
+        headersDone = true;
+      } else {
+        await dumpToCSV(output, papa.unparse([entryToArray(entry)], getPapaOptions()));
+      }
+    }
+  }
+  output.close();
+  console.log(`Generado archivo ${path.resolve(outputFile)}`);
+  return 0;
+}
+
+async function dumpToCSV(out: stream.Writable, s: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    out.write(s + '\n', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function getPapaOptions(): papa.UnparseConfig | undefined {
+  return undefined;
+}
+
+function getHeaders(): string[] {
+  return [
+    "Fecha",
+    "Autor",
+    "Colectivo",
+    "Texto"
+  ];
+}
+
+function entryToArray(entry: EventEntry): unknown[] {
+  return [
+    entry.date,
+    entry.author,
+    entry.code || "",
+    entry.text
+  ];
+}
+
+async function folderExists(pathLike: fs.PathLike, which: string): Promise<boolean> {
+  const e = await exists(pathLike);
+  if (!e) {
+    console.error(`No existe el directorio de ${which} ${pathLike}`);
+    return false;
+  }
+  const s = await stat(pathLike);
+  if (!s.isDirectory()) {
+    console.error(`La ruta de ${which} ${pathLike} no es un directorio`);
+    return false;
+  }
+  return true;
+}
+
+async function fileExists(pathLike: fs.PathLike): Promise<boolean> {
+  const e = await exists(pathLike);
+  if (e) {
+    console.error(`El archivo de salida ${pathLike} ya existe`);
+    return true;
+  }
+  return false;
+}
+
+async function* folderFiles(pathLike: string): AsyncIterableIterator<string> {
+  const files = await readdir(pathLike);
+  for (let i = 0; i < files.length; i++) {
+    const p = path.join(pathLike, files[i]);
+    if ((await stat(p)).isFile()) {
+      yield p;
+    }
+  }
+}
+
+interface EventEntry {
+  date: string;
+  author: string;
+  text: string;
+  code?: string;
+}
+
+const FECHA = "Fecha: ";
+const AUTOR = "Autor: ";
+const EVENTO = "Evento: ";
+
+async function* parseFile(file: string): AsyncIterableIterator<EventEntry> {
+  const input: stream.Readable = fs.createReadStream(file);
+  const rl = readline.createInterface({ input });
+  let currentEntry: Partial<EventEntry> = {};
+  for await (const l of rl) {
+    const line = l.trim();
+    if (line.startsWith(FECHA)) {
+      currentEntry.date = dayjs(line.substring(FECHA.length).trim(), "DD/MM/YY HH:mm").format("YYYY-MM-DDTHH:mm:ss");
+    } else if (line.startsWith(AUTOR)) {
+      currentEntry.author = line.substring(AUTOR.length);
+    } else if (line.startsWith(EVENTO)) {
+      currentEntry.text = line.substring(EVENTO.length);
+      const matches = /.*(\(\d{6}\)).*/g.exec(currentEntry.text);
+      if (matches && matches.length > 1) {
+        currentEntry.code = matches[1]!.substr(1, 6);
+      }
+      yield currentEntry as EventEntry;
+      currentEntry = {};
+    }
+  }
+}
+
+main()
+  .then((result: number) => {
+    process.exit(result);
+  }).catch((e: Error) => {
+    if (e.stack) {
+      console.error(e.stack);
+    }
+    console.error(`Error: ${e.message}`);
+  });
